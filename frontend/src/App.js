35abb19615b60af214262cc859a137513d79ac4c
Diego Maslo <diego@corewpress.com>
2024-01-25 22:27:36 -0500

    Added list of products + product page + router + other details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <main className='py-3'>
                 <Container>
                     <h1>eCommerce PoC using MERN</h1>
+                    <Outlet />
                 </Container>
             </main>
             <Footer />
